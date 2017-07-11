b3080c217aaf60d6125c1fbe60308c4d15dd8ea6
Mauro Massari <Mauro Massari@Mauro.lan>
2017-07-11 12:57:11 +0200

    aggiungi bottone

--- src/it/polito/tdp/prova/ProvaController.java
+++ src/it/polito/tdp/prova/ProvaController.java
@@ -1,5 +1,23 @@
+/**
+ * Sample Skeleton for 'Prova.fxml' Controller Class
+ */
+
 package it.polito.tdp.prova;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
 public class ProvaController {
-	
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
 }
